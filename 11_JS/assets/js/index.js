09a2eed30f98021d457aa949e6f6301588775d98
Nikita Kravchenko <nikita.kravchenko@pm.me>
2024-09-24 12:25:09 +0300

    Refactor fifth task

--- 11_JS/assets/js/index.js
+++ 11_JS/assets/js/index.js
@@ -59,11 +59,7 @@ console.log("");
 let triangleSize = Number(prompt("Введіть розмір трикутника:"));
 
 for (let i = 1; i <= triangleSize; i++) {
-  let line = "";
-  for (let j = 1; j <= i; j++) {
-    line += "*";
-  }
-  console.log(line);
+  console.log("*".repeat(i));
 }
 console.log("");
 
