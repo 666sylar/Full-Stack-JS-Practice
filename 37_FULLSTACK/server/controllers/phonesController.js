7443c39b332f593fbd6242d3b2b001220e908489
Nikita Kravchenko <nikita.kravchenko@pm.me>
2024-12-22 19:01:12 +0200

    37

---
+++ 37_FULLSTACK/server/controllers/phonesController.js
@@ -0,0 +1,165 @@
+const createHttpError = require('http-errors');
+const _ = require('lodash');
+const { Phone, Brand } = require('../db/models');
+
+module.exports.createPhone = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const createdPhone = await Phone.create(body);
+
+    if (!createdPhone) {
+      return next(createHttpError(400, 'Something went wrong'));
+    }
+
+    const preparedPhone = _.omit(createdPhone.get(), [
+      'createdAt',
+      'updatedAt',
+    ]);
+
+    res.status(201).send({ data: preparedPhone });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getPhones = async (req, res, next) => {
+  const { limit, offset } = req.pagination;
+
+  try {
+    const foundPhones = await Phone.findAll({
+      raw: true,
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+      limit,
+      offset,
+      order: ['id'],
+      include: {
+        model: Brand,
+        attributes: ['name'],
+      },
+    });
+
+    res.status(200).send({
+      data: foundPhones,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getPhoneById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const foundPhone = await Phone.findByPk(id, {
+      raw: true,
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+      include: {
+        model: Brand,
+        attributes: ['name'],
+      },
+    });
+
+    if (!foundPhone) {
+      return next(createHttpError(404, 'Phone not found'));
+    }
+
+    res.status(200).send({ data: foundPhone });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updatePhone = async (req, res, next) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+
+  try {
+    const [updatedPhonesCount, [updatedPhone]] = await Phone.update(body, {
+      where: { id },
+      raw: true,
+      returning: true,
+    });
+
+    if (!updatedPhonesCount) {
+      return next(createHttpError(404, 'Phone not found'));
+    }
+
+    const preparedPhone = _.omit(updatedPhone, ['createdAt', 'updatedAt']);
+
+    res.status(200).send({ data: preparedPhone });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateOrCreatePhone = async (req, res, next) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+
+  try {
+    const [updatedPhonesCount, [updatedPhone]] = await Phone.update(body, {
+      where: { id },
+      raw: true,
+      returning: true,
+    });
+
+    if (!updatedPhonesCount) {
+      body.id = id;
+      return next();
+    }
+
+    const preparedPhone = _.omit(updatedPhone, ['createdAt', 'updatedAt']);
+
+    res.status(200).send({ data: preparedPhone });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updatePhoneImage = async (req, res, next) => {
+  const {
+    file,
+    params: { id },
+  } = req;
+
+  try {
+    if (!file) {
+      return next(createHttpError(422, 'Image is required'));
+    }
+
+    const [updatedPhoneCount, [updatedPhone]] = await Phone.update(
+      { image: file.filename },
+      { where: { id }, raw: true, returning: true }
+    );
+
+    if (!updatedPhoneCount) {
+      return next(createHttpError(404, 'Phone Not Found'));
+    }
+
+    const preparedPhone = _.omit(updatedPhone, ['createdAt', 'updatedAt']);
+
+    res.status(200).send(preparedPhone);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deletePhone = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deletedPhonesCount = await Phone.destroy({ where: { id } });
+    if (!deletedPhonesCount) {
+      return next(createHttpError(404, 'Phone not found'));
+    }
+
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ 37_FULLSTACK/server/routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const phonesRouter = require('./phonesRouter');
+const brandsRouter = require('./brandsRouter');
+
+const router = Router();
+
+router.use('/phones', phonesRouter);
+router.use('/brands', brandsRouter);
+
+module.exports = router;
